0448c553dc0a5ac7de4e048492a9a2984048bb57
Samir Qureshi <samir.h.qureshi@gmail.com>
2023-07-22 12:10:33 +0500

    Implement basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,29 @@
+let operandA;
+let operator;
+let operandB;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, operator, b){
+return (operator === "+") ? add(a, b) :
+(operator === "-") ? subtract(a, b) :
+(operator === "*") ? multiply(a, b) :
+(operator === "/") ? divide(a, b):
+null;
+}
+
+console.log(operate(5, '*' , 5));
