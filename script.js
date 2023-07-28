232203f7c7f976b4f32f931b1361e6573317611f
Samir Qureshi <samir.h.qureshi@gmail.com>
2023-07-28 11:18:31 +0500

    Initial Release

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let operandA;
-let operator;
-let operandB;
+let operandA = '';
+let operator = null;
+let operandB = '';
+let equal = false;
+
+let bool = true;
+
 
 function add(a, b) {
     return a + b;
@@ -18,12 +22,68 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, operator, b){
-return (operator === "+") ? add(a, b) :
-(operator === "-") ? subtract(a, b) :
-(operator === "*") ? multiply(a, b) :
-(operator === "/") ? divide(a, b):
-null;
+function modulo(a, b) {
+    return a % b;
 }
 
-console.log(operate(5, '*' , 5));
+function operate(a, operator, b) {
+    return (operator === "+") ? add(a, b) :
+        (operator === "-") ? subtract(a, b) :
+            (operator === "*") ? multiply(a, b) :
+                (operator === "/") ? divide(a, b) :
+                (operator === "%") ? modulo(a, b) :
+                    null;
+}
+
+const digits = document.querySelectorAll('.buttons-container > .operand');
+const arithmetic = document.querySelectorAll('.buttons-container > .arithmetic');
+const result = document.querySelector('.buttons-container > #result');
+const ac = document.querySelector('.buttons-container > #ac');
+const display = document.querySelector('.display');
+
+function delAll(){
+    operandA = '';
+    operator = null;
+    operandB = '';
+    display.textContent = '';
+}
+
+digits.forEach(digit => digit.addEventListener('click', () => {
+    bool = true;
+    display.textContent += digit.textContent;
+
+    if (operator === null) {
+        operandA += digit.textContent;
+    }
+    else {
+        operandB += digit.textContent;
+        equal = true;
+    }
+}));
+
+arithmetic.forEach(arith => arith.addEventListener('click', () => {
+    if (bool === true) {
+        if (operandB !== '') {
+            let op = operate(Number(operandA), operator, Number(operandB));
+            display.textContent = `${op} ${arith.textContent}`;
+            operandA = op;
+            operator = arith.textContent;
+            operandB = '';
+        }
+        else if(operandA !== ''){
+            display.textContent += arith.textContent;
+            operator = arith.textContent;
+        }
+    }
+    bool = false;
+}));
+
+
+result.addEventListener('click', () => {
+    if(equal === true){
+    display.textContent = operate(Number(operandA), operator, Number(operandB));
+    }
+    equal = false;
+})
+
+ac.addEventListener('click', delAll);
